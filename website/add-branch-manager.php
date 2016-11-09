b0967f1fd1539630d3f6a70ee613502f4e3b59cb
Louis-Marie Matthews <internet657-github@yahoo.fr>
2016-11-09 23:38:04 +0000

    Allow company managers to update and add branch managers

---
+++ website/add-branch-manager.php
@@ -0,0 +1,101 @@
+<?php
+session_start();
+
+require( 'classes/stores/BranchManager.php' );
+require( 'functions/html.php' );
+require( 'functions/authorizations.php' );
+
+checkIfCompanyManager();
+
+$formErrors = array();
+
+$bm = new BranchManager;
+$isValid = true;
+try {
+  $bm->setPersonId( getPost( 'person-id' ) );
+}
+catch ( DomainException $e ) {
+  $isValid = false;
+  $formErrors[] = $e->getMessage();
+}
+try {
+  $bm->setWage( getPost( 'wage' ) );
+}
+catch ( DomainException $e ) {
+  $isValid = false;
+  $formErrors[] = $e->getMessage();
+}
+try {
+  $bm->setBranchId( getPost( 'branch-id' ) );
+}
+catch( DomainException $e ) {
+  $isValid = false;
+  $formErrors[] = $e->getMessage();
+}
+try {
+  $sql = '
+    INSERT INTO BranchManager ( PersonId, BranchId, Wage, SortCode,
+      AccountNumber )
+    VALUES ( ?, ?, ?, ?, ? );
+  ';
+  $parameters = array(
+    $bm->getPersonId(),
+    $bm->getBranchId(),
+    $bm->getWage(),
+    $bm->getSortCode(),
+    $bm->getAccountNumber()
+  );
+  Database::query( $sql, $parameters );
+  $title = 'New Branch Manager Added';
+  $message = 'You successfully added the details of a nez
+    branch manager.';
+  displayMessagePage( $message, $title );
+}
+catch( Exception $e ) {
+  $formErrors[] = $e->getMessage();
+}
+?>
+<!doctype html>
+<html>
+  <head>
+    <?php displayHead() ?>
+    <title>Add New Branch Manager</title>
+  </head>
+  <body>
+    <main>
+      <?php displayErrors( $formErrors ) ?>
+      <table>
+        <tr>
+          <td>
+            <label for="person-id" form="form">Person Id</label>
+          </td>
+          <td>
+            <input form="form" id="person-id" name="person-id" 
+              type="text" value="<?php echo( $bm->getPersonId() ) ?>"/>
+          </td>
+        </tr>
+        <tr>
+          <td>
+            <label for="branch-id" form="form">Branch Id</label>
+          </td>
+          <td>
+            <input form="form" id="branch-id" name="branch-id" type="text"
+            value="<?php echo( $bm->getBranchId() ) ?>" />
+          </td>
+        </tr>
+        <tr>
+          <td>
+            <label for="wage" form="form">Wage</label>
+          </td>
+          <td>
+            <input form="form" id="wage" name="wage" type="text" 
+            value="<?php echo( $bm->getWage() ) ?>" />
+          </td>
+        </tr>
+      </table>
+      <form action="#" id="form" method="POST">
+        <button type="submit">Update</button>
+      </form>
+    </main>
+  </body>
+</html>
